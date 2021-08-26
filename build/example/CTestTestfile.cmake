9c03b316fd49edca40077deabee44456734d8a59
pinyu250174 <408350121@gms.tku.edu.tw>
2021-08-26 16:20:40 +0800

    for lesson one add workspace add package add hellow_world.cpp

---
+++ build/example/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pinyu/test/src/example
+# Build directory: /home/pinyu/test/build/example
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
